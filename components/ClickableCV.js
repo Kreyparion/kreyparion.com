5f7ba9475a5e6093414e073644b72feaac195054
Kreyparion <kreyparion@gmail.com>
2024-04-30 21:28:05 +0200

    update CV

--- components/ClickableCV.js
+++ components/ClickableCV.js
@@ -10,7 +10,7 @@ export default function ClickableCV() {
             <h1 className={styles.AboutMe}>About Me</h1>
             <div className={styles.lines}>
               <span className={styles.MyName}>Hi! I{"'"}m Julien. </span>
-              <span>I{"'"}m an AI Research Scientist specialised in <b>Reinforcement Learning</b>, <b>Pathfinding</b> and <b>Optimisation Methods</b>. </span>
+              <span>I{"'"}m an AI Research Scientist specialised in <b>Reinforcement Learning</b>, <b>Pathfinding</b> and <b>Optimization Methods</b>. </span>
               <span className={styles.line}>I value <u>Open Source</u> and <u>Open Knowledge</u>. </span>
               <span className={styles.line}>I like pushing the boundaries of the State Of The Art and combining <b>Deep Learning</b>, <b>Algorithmics Methods</b> and <b>Maths Principles</b> to build powerful solutions for specific problems.</span>
               <span className={styles.line}>This website is for me a way to better share my projects and reach more people.</span>
